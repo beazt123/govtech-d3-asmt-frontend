07ff4f36bb10904f4a2ee4f96d9daf8cb7d6f1d7
beazt123 <lohzhixuan@live.com>
2021-06-28 01:27:46 +0800

    Removed zombie code and added error messages for empty and invalid url

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -3,22 +3,13 @@
     <h2>URL Shortening Service</h2>
     <div class="upload-section">
       <el-input placeholder="Input URL here" v-model="input"></el-input>
-      <!-- <input type="text" id="lname" name="lname"> -->
     </div>
 
     <div class="upload-section">
       <el-button size="small" type="primary" round @click="submitLink">Shorten!</el-button>
     </div>
     <el-divider class="divider">Result below</el-divider>
-    <!-- <span>{{text}}</span> -->
     <el-link v-bind:href="text" target="_blank">{{text}}</el-link>
-
-
-    <!-- <div class="button-holder">
-      <el-button type="primary" v-on:click="searchForEmployees" round size="small">Search</el-button>
-    </div> -->
-
-    
   </div>
 </template>
 
@@ -32,14 +23,8 @@ export default {
   methods:{
     submitLink() {
       console.log("Submit link function called");
-      if (this.input != null){
+      if (this.input != ""){
         console.log("Link detected");
-        // console.log(this.input)
-        
-        // this.employeeData 
-        // this.axios.get("http://localhost:5000/").then((response) => {
-        //   console.log(response.data)
-        // })
         this.axios({
           method: 'post',
           url: 'https://url-shortening-srv.herokuapp.com/shortener',
@@ -48,56 +33,29 @@ export default {
           },
           headers: { 
             "Content-Type": "application/json" 
-          }//"Access-Control-Allow-Origin": "*" 
+          }
         }).then( (response) => {
           //handle success
           console.log(response.data);
           var resData = response.data.data;
-          console.log(resData["url-short"]); //prints out properly
+          console.log(resData["url-short"]); 
           this.text = resData["url-short"];
           
           
         }).catch( (error) => {
           //handle error
           console.log(error);
-        //   this.$message.error(error.response.data.description);
+          this.$message.error(error.response.data.description);
         });
       }
       else {
-        this.$message.error("Please attach a CSV file");
+        this.$message.error("Please enter a proper URL");
       }
-    },
-    searchForEmployees: function(){
-      var backEndSortOperator;
-      if (this.sortDirection){
-        backEndSortOperator = "%2b";
-      }else{
-        backEndSortOperator = "%2d";
-      }
-      this.axios({
-        method: 'get',
-        url: 'http://localhost:5000/users?' 
-          + 'maxSalary=' + this.salaryRange[1]
-          + '&minSalary=' + this.salaryRange[0]
-          + '&offset=' + ((this.currentPage - 1) * 30)
-          + '&limit=' + 30
-          + '&sort=' + backEndSortOperator + this.sortBy
-      }).then( response => {
-        this.tableData = [];
-        var users = response.data.data.results;
-        console.log(users);
-        this.tableData = users;
-      }).catch( (error) => {
-        console.log(error);
-        // console.log(error.response);
-        // this.$message.error(error.response.data.description);
-      });
     }
   },
   data () {
     return {
       link: "",
-      employeeData: null,
       text: "",
       input:""
     }
